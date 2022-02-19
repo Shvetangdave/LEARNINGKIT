836ae4bd39775f910241a113e8ab3514c45972ee
Devarsh <87245954+Shvetangdave@users.noreply.github.com>
2022-02-19 13:48:04 -0500

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,11 @@
+var Score1 = 0;
+
+function n9(){
+  document.getElementById("cow").innerHTML='CORRECT!!!';
+  window.location="setest";
+}
+
+function n5(){
+  
+  document.getElementById("cow").innerHTML='WRONG TRY AGAIN';
+}
